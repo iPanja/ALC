7d7db8cc6d43a377e30222803f811d0e0960d899
iPanja <fhenneman@comcast.net>
2019-06-22 17:18:13 -0400

    Add files via upload

---
+++ ALC/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace ALC {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.locateClientPicture = new System.Windows.Forms.PictureBox();
+            this.initiateButton = new MetroFramework.Controls.MetroButton();
+            this.outsidePanel = new System.Windows.Forms.Panel();
+            this.insidePanel = new System.Windows.Forms.Panel();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.monitorQueuePicture = new System.Windows.Forms.PictureBox();
+            this.pbKeyTextbox = new MetroFramework.Controls.MetroTextBox();
+            this.pbHelpButton = new MetroFramework.Controls.MetroButton();
+            this.pbCheckbox = new MetroFramework.Controls.MetroCheckBox();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.disableCheckbox = new MetroFramework.Controls.MetroCheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.locateClientPicture)).BeginInit();
+            this.outsidePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.monitorQueuePicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // locateClientPicture
+            // 
+            this.locateClientPicture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.locateClientPicture.Image = ((System.Drawing.Image)(resources.GetObject("locateClientPicture.Image")));
+            this.locateClientPicture.Location = new System.Drawing.Point(103, 117);
+            this.locateClientPicture.Name = "locateClientPicture";
+            this.locateClientPicture.Size = new System.Drawing.Size(25, 23);
+            this.locateClientPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.locateClientPicture.TabIndex = 1;
+            this.locateClientPicture.TabStop = false;
+            // 
+            // initiateButton
+            // 
+            this.initiateButton.Location = new System.Drawing.Point(68, 80);
+            this.initiateButton.Name = "initiateButton";
+            this.initiateButton.Size = new System.Drawing.Size(320, 23);
+            this.initiateButton.TabIndex = 2;
+            this.initiateButton.Text = "Initiate";
+            this.initiateButton.UseSelectable = true;
+            this.initiateButton.Click += new System.EventHandler(this.initiateButton_Click);
+            // 
+            // outsidePanel
+            // 
+            this.outsidePanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.outsidePanel.Controls.Add(this.insidePanel);
+            this.outsidePanel.Location = new System.Drawing.Point(0, 362);
+            this.outsidePanel.Name = "outsidePanel";
+            this.outsidePanel.Size = new System.Drawing.Size(445, 10);
+            this.outsidePanel.TabIndex = 3;
+            // 
+            // insidePanel
+            // 
+            this.insidePanel.BackColor = System.Drawing.Color.Cyan;
+            this.insidePanel.Location = new System.Drawing.Point(0, 0);
+            this.insidePanel.Name = "insidePanel";
+            this.insidePanel.Size = new System.Drawing.Size(60, 10);
+            this.insidePanel.TabIndex = 4;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.metroLabel1.Location = new System.Drawing.Point(134, 117);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(88, 19);
+            this.metroLabel1.TabIndex = 4;
+            this.metroLabel1.Text = "Locate Client";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.metroLabel2.Location = new System.Drawing.Point(135, 156);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(104, 19);
+            this.metroLabel2.TabIndex = 5;
+            this.metroLabel2.Text = "Monitor Queue";
+            // 
+            // monitorQueuePicture
+            // 
+            this.monitorQueuePicture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.monitorQueuePicture.Image = ((System.Drawing.Image)(resources.GetObject("monitorQueuePicture.Image")));
+            this.monitorQueuePicture.Location = new System.Drawing.Point(103, 152);
+            this.monitorQueuePicture.Name = "monitorQueuePicture";
+            this.monitorQueuePicture.Size = new System.Drawing.Size(25, 23);
+            this.monitorQueuePicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.monitorQueuePicture.TabIndex = 6;
+            this.monitorQueuePicture.TabStop = false;
+            // 
+            // pbKeyTextbox
+            // 
+            // 
+            // 
+            // 
+            this.pbKeyTextbox.CustomButton.Image = null;
+            this.pbKeyTextbox.CustomButton.Location = new System.Drawing.Point(219, 1);
+            this.pbKeyTextbox.CustomButton.Name = "";
+            this.pbKeyTextbox.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.pbKeyTextbox.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.pbKeyTextbox.CustomButton.TabIndex = 1;
+            this.pbKeyTextbox.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.pbKeyTextbox.CustomButton.UseSelectable = true;
+            this.pbKeyTextbox.CustomButton.Visible = false;
+            this.pbKeyTextbox.Lines = new string[0];
+            this.pbKeyTextbox.Location = new System.Drawing.Point(113, 305);
+            this.pbKeyTextbox.MaxLength = 32767;
+            this.pbKeyTextbox.Name = "pbKeyTextbox";
+            this.pbKeyTextbox.PasswordChar = '\0';
+            this.pbKeyTextbox.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.pbKeyTextbox.SelectedText = "";
+            this.pbKeyTextbox.SelectionLength = 0;
+            this.pbKeyTextbox.SelectionStart = 0;
+            this.pbKeyTextbox.ShortcutsEnabled = true;
+            this.pbKeyTextbox.Size = new System.Drawing.Size(241, 23);
+            this.pbKeyTextbox.TabIndex = 7;
+            this.pbKeyTextbox.UseSelectable = true;
+            this.pbKeyTextbox.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.pbKeyTextbox.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            this.pbKeyTextbox.TextChanged += new System.EventHandler(this.pbKeyTextbox_TextChanged);
+            // 
+            // pbHelpButton
+            // 
+            this.pbHelpButton.Location = new System.Drawing.Point(360, 304);
+            this.pbHelpButton.Name = "pbHelpButton";
+            this.pbHelpButton.Size = new System.Drawing.Size(28, 23);
+            this.pbHelpButton.TabIndex = 8;
+            this.pbHelpButton.Text = "?";
+            this.pbHelpButton.UseSelectable = true;
+            this.pbHelpButton.Click += new System.EventHandler(this.pbHelpButton_Click);
+            // 
+            // pbCheckbox
+            // 
+            this.pbCheckbox.AutoSize = true;
+            this.pbCheckbox.Location = new System.Drawing.Point(81, 305);
+            this.pbCheckbox.Name = "pbCheckbox";
+            this.pbCheckbox.Size = new System.Drawing.Size(26, 15);
+            this.pbCheckbox.TabIndex = 9;
+            this.pbCheckbox.Text = " ";
+            this.pbCheckbox.UseSelectable = true;
+            this.pbCheckbox.CheckedChanged += new System.EventHandler(this.pbCheckbox_CheckedChanged);
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.metroLabel3.Location = new System.Drawing.Point(68, 283);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(156, 19);
+            this.metroLabel3.TabIndex = 10;
+            this.metroLabel3.Text = "Pushbullet Notifications:";
+            // 
+            // disableCheckbox
+            // 
+            this.disableCheckbox.AutoSize = true;
+            this.disableCheckbox.Location = new System.Drawing.Point(109, 190);
+            this.disableCheckbox.Name = "disableCheckbox";
+            this.disableCheckbox.Size = new System.Drawing.Size(130, 15);
+            this.disableCheckbox.TabIndex = 11;
+            this.disableCheckbox.Text = "Immediately Disable";
+            this.disableCheckbox.UseSelectable = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(445, 369);
+            this.Controls.Add(this.disableCheckbox);
+            this.Controls.Add(this.metroLabel3);
+            this.Controls.Add(this.pbCheckbox);
+            this.Controls.Add(this.pbHelpButton);
+            this.Controls.Add(this.pbKeyTextbox);
+            this.Controls.Add(this.monitorQueuePicture);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.outsidePanel);
+            this.Controls.Add(this.initiateButton);
+            this.Controls.Add(this.locateClientPicture);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Resizable = false;
+            this.Text = "Auto League Client";
+            ((System.ComponentModel.ISupportInitialize)(this.locateClientPicture)).EndInit();
+            this.outsidePanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.monitorQueuePicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox locateClientPicture;
+        private MetroFramework.Controls.MetroButton initiateButton;
+        private System.Windows.Forms.Panel outsidePanel;
+        private System.Windows.Forms.Panel insidePanel;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private System.Windows.Forms.PictureBox monitorQueuePicture;
+        private MetroFramework.Controls.MetroTextBox pbKeyTextbox;
+        private MetroFramework.Controls.MetroButton pbHelpButton;
+        private MetroFramework.Controls.MetroCheckBox pbCheckbox;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroCheckBox disableCheckbox;
+    }
+}
+
---
+++ ALC/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using PushbulletSharp;
+using PushbulletSharp.Models.Requests;
+
+namespace ALC {
+    public partial class Form1 : MetroFramework.Forms.MetroForm {
+        public Process leagueProcess;
+        public int plus = 2;
+        public Timer timer = new Timer();
+        public Timer monitorTimer = new Timer();
+        public bool initiated = false;
+        Image checkmark = Image.FromFile(Application.StartupPath + "/checkmark.png");
+        Image x = Image.FromFile(Application.StartupPath + "/x.png");
+        Color fPixel = Color.FromArgb(0, 133, 163, 164);
+        Color bPixel = Color.FromArgb(0, 30, 37, 42);
+        const int Press = 0x02;
+        const int Unpress = 0x04;
+        PushbulletClient pbClient;
+
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rect rect);
+        [DllImport("user32.dll")]
+        public static extern bool PrintWindow(IntPtr hWnd, IntPtr hdcBlt, int nFlags);
+        [DllImport("user32.dll")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+        [DllImport("user32.dll", EntryPoint = "SetCursorPos")]
+        private static extern bool SetCursorPos(int x, int y);
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Rect {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+
+        public Form1() {
+            InitializeComponent();
+            timer.Tick += new EventHandler(move);
+            timer.Interval = 3;
+            monitorTimer.Tick += new EventHandler(scan);
+            monitorTimer.Interval = 5000;
+            pbCheckbox.Checked = Properties.Settings.Default.pbNotify;
+            pbKeyTextbox.Text = Properties.Settings.Default.pbKey;
+            pbKeyTextbox.Enabled = pbCheckbox.Checked;
+        }
+
+        //Events
+        private void initiateButton_Click(object sender, EventArgs e) {
+            if (!initiated) {
+                toggleButton();
+                if (stepFindClient()) {
+                    locateClientPicture.Image = checkmark;
+                    stepMonitorQueue();
+                }else {
+                    toggleButton();
+                }
+            }
+            else {
+                toggleButton();
+            }
+        }
+        private void pbHelpButton_Click(object sender, EventArgs e) {
+            System.Diagnostics.Process.Start("https://www.pushbullet.com/#settings/account");
+        }
+        private void pbKeyTextbox_TextChanged(object sender, EventArgs e) {
+            Properties.Settings.Default.pbKey = pbKeyTextbox.Text;
+            Properties.Settings.Default.Save();
+        }
+        private void pbCheckbox_CheckedChanged(object sender, EventArgs e) {
+            Properties.Settings.Default.pbNotify = pbCheckbox.Checked;
+            Properties.Settings.Default.Save();
+            pbKeyTextbox.Enabled = pbCheckbox.Checked;
+        }
+        //Functions
+        public bool stepFindClient() {
+            Process temp = findProcess();
+            if (temp == null) {
+                showBalloon("Could not locate a single league instance", SystemIcons.Exclamation);
+                return false;
+            }
+            else {
+                leagueProcess = temp;
+                return true;
+            }
+        }
+        public void stepMonitorQueue() {
+            //Initiate Progress Bar
+            monitorQueuePicture.Image = checkmark;
+            monitorTimer.Start();
+        }
+        void toggleButton() {
+            if (initiated) {
+                //The process has ended
+                initiateButton.Text = "Initiate";
+                timer.Stop();
+                monitorTimer.Stop();
+                locateClientPicture.Image = x;
+                monitorQueuePicture.Image = x;
+                insidePanel.Left = 0;
+            }else {
+                initiateButton.Text = "Cancel";
+                timer.Start();
+            }
+            initiated = !initiated;
+        }
+        Process findProcess() {
+            Process[] procs = Process.GetProcessesByName("leagueclientux");
+            if (procs.Length != 1) {
+                return null;
+            }
+            else {
+                return procs[0];
+            }
+        }
+        void showBalloon(string body, Icon icon) {
+            NotifyIcon notifyIcon = new NotifyIcon();
+            notifyIcon.Visible = true;
+            notifyIcon.Icon = icon;
+
+            notifyIcon.BalloonTipTitle = "Auto League Client";
+            notifyIcon.BalloonTipText = body;
+            notifyIcon.ShowBalloonTip(3000);
+        }
+        public void move(object sender, EventArgs e) {
+            insidePanel.Left = insidePanel.Left + plus;
+            if (insidePanel.Left > 385) {
+                plus = -2;
+            }
+            else if (insidePanel.Left < 0) {
+                plus = 2;
+            }
+        }
+        public void scan(object sender, EventArgs e) {
+            Tuple<Bitmap, Rect> values = CaptureScreenshot();
+            Bitmap screenshot = values.Item1;
+            Rect rect = values.Item2;
+            //screenshot.Save(Application.StartupPath + "/Screenshot.png", ImageFormat.Png);
+            int x = Convert.ToInt32(screenshot.Width / 2);
+            int y = Convert.ToInt32(screenshot.Height * 0.75);
+            Color pixel = screenshot.GetPixel(x, y);
+            //Console.Out.WriteLine("A" + pixel.A + " R" + pixel.R + " G" + pixel.G + " B" + pixel.B);
+            if(pixcmp(pixel, fPixel) || pixcmp(pixel, bPixel)) {
+                //Accept the match
+                Console.Out.WriteLine("Match found!"); 
+                AcceptMatch(rect.left + x, rect.top + y);
+                handleNotifications();
+                toggleButton();
+            }
+        }
+        public void AcceptMatch(int x, int y) {
+            SetForegroundWindow(leagueProcess.MainWindowHandle);
+            SetCursorPos(x, y);
+            mouse_event(Press, 0, 0, 0, 0);
+            mouse_event(Unpress, 0, 0, 0, 0);
+        }
+        public void pushNotification(string message, string pbKey) {
+            if (pbClient == null) {
+                pbClient = new PushbulletClient(pbKey);
+            }
+            if(pbClient.CurrentUsersInformation() == null) {
+                showBalloon("Your Pushbullet key is invalid", SystemIcons.Error);
+                return;
+            }
+            PushNoteRequest note = new PushNoteRequest {
+                Title = "ALC",
+                Body = message,
+            };
+            pbClient.PushNote(note);
+        }
+        public Tuple<Bitmap, Rect> CaptureScreenshot() {
+            Rect rect = new Rect();
+            GetWindowRect(leagueProcess.MainWindowHandle, ref rect);
+            Bitmap bmp = new Bitmap(rect.right - rect.left, rect.bottom - rect.top, PixelFormat.Format32bppArgb);
+            Graphics graphics = Graphics.FromImage(bmp);
+            IntPtr bitmap = graphics.GetHdc();
+            PrintWindow(leagueProcess.MainWindowHandle, bitmap, 0);
+            graphics.ReleaseHdc(bitmap);
+            graphics.Dispose();
+            return Tuple.Create(bmp, rect);
+        }
+        public void handleNotifications() {
+            string temp = pbKeyTextbox.Text;
+            if (!string.IsNullOrWhiteSpace(temp) && pbCheckbox.Checked) {
+                pushNotification("A match has been found!", pbKeyTextbox.Text);
+            }
+        }
+        public bool pixcmp(Color p1, Color p2) {
+            return p1.R == p2.R && p1.G == p2.G && p1.B == p2.B;
+        }
+    }
+}
